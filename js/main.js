d9e8ea049e95e23a133013449067cb97128dc8c7
znaura <zaizafunnaura@gmail.com>
2023-10-22 20:13:19 +0700

    lass

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'), navToggle = document.getElementById('nav-toggle'), navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
-        navMenu.classList.add('show-menu')
-    })
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', ()=>{
-        navMenu.classList.remove('show-menu')
-    })
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
@@ -22,29 +24,30 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    //when clicked on each nav__link, it removes the show-menu class
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills__content'),skillsHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
-function toggleSkills(){
-    let itemClass = this.parentNode.className
+      function toggleSkills() {
+        let itemClass = this.parentNode.className
 
-    for(i=0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
-    if(itemClass == 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
-    }
-}
-
-skillsHeader.forEach((el) =>{
-    el.addEventListener('click', toggleSkills)
-})
-
+        for(i = 0; i < skillsContent.length; i++) {
+          skillsContent[i].className = 'skills__content skills__close'
+        }
+        if(itemClass === 'skills__content skills__close'){
+          this.parentNode.className = 'skills__content skills_open'
+        }
+      }
+      
+      skillsHeader.forEach((el) => {
+        el.addEventListener('click', toggleSkills)
+      })
+      
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll('.services__modal'),
       modalBtns = document.querySelectorAll('.services__button'),
